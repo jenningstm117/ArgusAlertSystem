1a720341e352e9bf11f902cf24be3c6958889975
Michael Jennings <jenningstm117@gmail.com>
2014-10-23 16:36:49 -0400

    Added version of Argus to use PIR module for detecting motion instead of images. Haven't tested it yet, so it probably doesn't work

---
+++ ArgusPIR.py
@@ -0,0 +1,166 @@
+import os, time, io, picamera
+import RPi.GPIO as GPIO
+import MailWrapper
+from datetime import datetime
+from PIL import Image
+
+
+class Argus(object):
+    def __init__(self, root_dir, email_creds):
+        self.alert_active = False
+        self.root_dir = root_dir
+        self.email_username, self.email_password = email_creds
+        self.PIR_PIN = None
+        self.video_stream = None
+        self.camera = None
+        self.motion_detected = None
+        self.last_motion = None
+        self.current_file_path = None
+
+    def Start(self):
+        self.initEmail()
+        self.initPirModule()
+        self.init_camera()
+        time.sleep(2)
+        self.initVideoStream()
+        time.sleep(5)
+        ## Sit in a loop checking for motion every couple seconds
+        try:
+            while 1:
+                time.sleep(10)
+                if self.alert_active and int(time.time())-self.last_motion>60:
+                    self.deactivateAlert()
+        except KeyboardInterrupt:
+            print 'Exiting . . .'
+            GPIO.cleanup()
+
+    ## Setup email notifications
+    def initEmail(self):
+        self.email_manager = MailWrapper.MailWrapper(self.email_username, self.email_password)
+        self.email_manager.addReceiver('jenningstm117@gmail.com')
+
+        self.alert_active_subject = 'Argus Alert Activated'
+        self.alert_deactive_subject = 'Argus Alert Deactivated'
+
+    ##Setup Pir motion detection device
+    def initPirModule(self):
+        GPIO.setmode(GPIO.BCM)
+        self.PIR_PIN = 7
+        GPIO.setup(self.PIR_PIN, GPIO.IN)
+
+        time.sleep(2)
+        GPIO.add_event_detect(self.PIR_PIN, GPIO.RISING, callback = self.motionDetected_callback)
+
+    def motionDetected_callback(self):
+        self.motion_detected = True
+        self.last_motion = time.time()
+
+        if not self.alert_active:
+            self.activateAlert()
+
+
+    ## Create PiCamera Camera instance which Argus will use for recording any images/videos
+    def init_camera(self):
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = (100, 75)
+
+    ## Get initial image to use in comparisons for motion detection
+    def initImage(self):
+        self.current_image, self.current_pil_buffer = self.captureImage()
+
+    ## Capture image and store in memory as an actual image as well as a buffer of pixel values using PIL.Image
+    def captureImage(self):
+        stream = io.BytesIO()
+        self.camera.capture(stream, format='jpeg', use_video_port=True)
+        # "Rewind" the stream to the beginning so we can read its content
+        stream.seek(0)
+        image = Image.open(stream)
+        stream.close()
+        return image
+
+    ## Setup video recording to circular stream in memory, so the past 5 seconds of video will always be available
+    def initVideoStream(self):
+        self.video_stream = picamera.PiCameraCircularIO(self.camera, seconds=5)
+        self.camera.start_recording(self.video_stream, format='h264')
+
+    ## Record video to file . . . duh
+    def recordVideoToFile(self, file_name):
+        self.camera.start_recording(file_name)
+
+    ## Stop recording video, so that recording to a different location can be done with the same PiCamera object
+    def stopVideoRecording(self):
+        self.camera.stop_recording()
+
+    ## take the 5 second video stream and combine it with a video file, into one new file
+    def persistVideo(self, newFilename, tempFilename):
+        for frame in self.video_stream.frames:
+            if frame.header:
+                self.video_stream.seek(frame.position)
+                break
+        with open(tempFilename, 'rb') as tempFile, open(newFilename, 'wb') as newFile:
+            while True:
+                data = self.video_stream.read1()
+                if not data:
+                    break
+                newFile.write(data)
+            for chunk in iter(lambda: tempFile.read(1024), b""):
+                newFile.write(chunk)
+        os.remove(tempFilename)
+
+    ## When an alert is activated, get the file path based on current date and time, save the image
+    ## that captured the motion, send the image in an email, stop recording to the circular stream, and
+    ## start recording to a file
+    def activateAlert(self):
+        self.alert_active = True
+        self.current_file_path = self.getFilePath()
+        self.saveImage('%s%s'%(self.current_file_path, self.getFilename('alertActivated')))
+        self.sendAlertEmail('alertActivated', '%s%s'%(self.current_file_path, self.getFilename('alertActivated')))
+        self.stopVideoRecording()
+        self.recordVideoToFile('%s%s'%(self.current_file_path, 'temp.h264'))
+
+    ## When an alert is deactivated, save the image, send it in an email, persist the final video file,
+    ## and start watching for motion again
+    def deactivateAlert(self):
+        self.alert_active = False
+        self.saveImage('%s%s'%(self.current_file_path, self.getFilename('alertDeactivated')))
+        self.sendAlertEmail('alertDeactivated', '%s%s'%(self.current_file_path, self.getFilename('alertDeactivated')))
+        self.stopVideoRecording()
+        self.persistVideo('%s%s'%(self.current_file_path, self.getFilename('video')), '%stemp.h264'%self.current_file_path)
+        self.initVideoStream()
+
+    ## Construct the alert email to be sent
+    def sendAlertEmail(self, type, image_file):
+        if type == 'alertActivated':
+            self.email_manager.createMail(self.alert_active_subject, '')
+            self.email_manager.attachFile(image_file)
+            self.email_manager.sendMail()
+        elif type == 'alertDeactivated':
+            self.email_manager.createMail(self.alert_deactive_subject, '')
+            self.email_manager.attachFile(image_file)
+            self.email_manager.sendMail()
+
+    ## Save the current image in memory to a file
+    def saveImage(self, filename):
+        self.captureImage().save(filename)
+
+    ## Get the file path based on current date and time
+    def getFilePath(self):
+        now = datetime.now()
+        year = now.year
+        month = now.month
+        day = now.day
+        hour = now.hour
+        minute=now.minute
+        filePath = '%s/%s/%s/%s/%02d:%02d/'%(self.root_dir, year, month, day, hour, minute)
+        if not os.path.exists(filePath):
+            os.makedirs(filePath)
+        return filePath
+
+    ## Get filename based on alert status and media type
+    def getFilename(self, fileType):
+        if fileType=='alertActivated':
+            return 'alertActivated.jpeg'
+        elif fileType=='alertDeactivated':
+            return 'alertDeactivated.jpeg'
+        elif fileType=='video':
+            return 'video.h264'
--- Main.py
+++ Main.py
@@ -1,9 +1,13 @@
-import Argus
+import Argus, ArgusPIR
 
 def Main():
-    email_creds = ('********@gmail.com', '**********')
-    argusAlert = Argus('/home/pi', email_creds)
-    argusAlert.Start()
+    email_creds = ('********@gmail.com', '********')
+    #argusAlert = Argus('/home/pi', email_creds)
+    #argusAlert.Start()
+
+    argusPirAlert = ArgusPIR('/home/pi', email_creds)
+    argusPirAlert.Start()
+
 
 if __name__ == '__main__':
     Main()
